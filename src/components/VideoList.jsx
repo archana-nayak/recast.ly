9c2678e4680b34cc9b4b4883f1751b58429c9016
archana-nayak <archana.nayak@gmail.com>
2017-12-13 19:24:45 -0800

    create stateless video components

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,8 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+var VideoList = (props) => (
+  <div>
+    {props.videos.map(video => {
+      return <VideoListEntry item={video}/>;
+    })}
   </div>
 );
 
@@ -17,3 +15,19 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 window.VideoList = VideoList;
+//console.log('In videolist ', window.exampleVideoData);
+
+// <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+
+
+// <div className="video-list">
+//     this.props.videos.map(function(video){
+      
+//       <VideoListEntry videoItem={video} />
+
+//     });
+//   </div>
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,11 +1,11 @@
-var VideoPlayer = () => (
+var VideoPlayer = (props) => (
   <div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" allowFullScreen></iframe>
+      <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ?" allowFullScreen></iframe>
     </div>
     <div className="video-player-details">
-      <h3>Video Title</h3>
-      <div>Video Description</div>
+      <h5>{props.video.snippet.title}</h5>
+      <div><h6>{props.video.snippet.description}</h6></div>
     </div>
   </div>
 );
